d0b96796e029f0ce949b264eb537bbe2f142e590
Hilda Beltrán <hildabeltran@Hildas-MacBook-Pro.local>
2021-05-21 01:40:42 -0500

    Cargar código

---
+++ empleado.h
@@ -0,0 +1,29 @@
+#ifndef EMPLEADO_H
+#define EMPLEADO_H
+#include <iostream>
+
+using namespace std;
+
+class Empleado{
+    private:
+        string idEmpleado, nombreEmpleado, hospitalEmpleado;
+    
+    public:
+        string get_id(){ return idEmpleado;}
+        string get_nombre(){ return nombreEmpleado;}
+        string get_hospital(){ return hospitalEmpleado;}
+        Empleado(string id, string nombre, string hospital);
+        void bienvenida();
+};
+
+Empleado::Empleado(string id, string nombre, string hospital){
+    idEmpleado = id;
+    nombreEmpleado = nombre;
+    hospitalEmpleado = hospital;
+}
+
+void Empleado::bienvenida(){
+    cout << "Hola " << nombreEmpleado << "!" << endl;
+}
+
+#endif
